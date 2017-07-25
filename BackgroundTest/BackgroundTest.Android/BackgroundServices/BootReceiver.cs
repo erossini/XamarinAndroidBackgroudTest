35dae1b886f478f373b230df3fee35f6dae65ca5
Enrico Rossini <e.rossini@portico.com>
2017-07-25 17:06:08 +0100

    First update

---
+++ BackgroundTest/BackgroundTest.Android/BackgroundServices/BootReceiver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace BackgroundTest.Droid.BackgroundServices
+{
+    public class BootReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            Intent i = new Intent(context, typeof(MainActivity));
+            i.AddFlags(ActivityFlags.NewTask);
+            context.StartActivity(i);
+        }
+    }
+}
